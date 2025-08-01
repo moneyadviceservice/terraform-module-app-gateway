c7b6bf3969b7c89e5f08169303bc31741b5aa2a2
Zoe Cope <zoe.cope@hmcts.net>
2025-08-01 12:03:29 +0100

    rm provider

--- data.tf
+++ data.tf
@@ -1,13 +1,1 @@
 data "azurerm_client_config" "current" {}
-
-data "azurerm_key_vault" "main" {
-  provider            = azurerm.kv
-  name                = var.vault_name
-  resource_group_name = var.key_vault_resource_group
-}
-
-data "azurerm_key_vault_secret" "certificate" {
-  provider     = azurerm.kv
-  name         = var.ssl_certificate_name != null ? var.ssl_certificate_name : "null"
-  key_vault_id = data.azurerm_key_vault.main.id
-}
